9887e454920449a91b5f8c5f7495d63490638ec5
Janne Alatalo <janne.alatalo@jamk.fi>
2015-05-06 14:16:14 +0300

    Add port parser test

--- test-parser.py
+++ test-parser.py
@@ -9,5 +9,11 @@ class TestParsePortParams(unittest.TestCase):
 
         self.assertItemsEqual(res, exp)
 
+    def test_string_param_with_hostip(self):
+        exp = {'80/tcp': [{'HostIp': '192.168.1.101', 'HostPort': '80'}]}
+        res = dockerimpimp.parse_port_params("192.168.1.101:80:80")
+
+        self.assertDictContainsSubset(res, exp)
+
 if __name__ == "__main__":
     unittest.main()
